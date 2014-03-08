2af802d2c8e36e0922112efd1926a01a74b10528
Abdullah Alansari <ahimta@gmail.com>
2014-03-09 01:44:25 +0300

    fixed: Trade#ideas_count (the counter_cache) defaulting to null rather than 0

--- db/migrate/20140307102944_create_trades.rb
+++ db/migrate/20140307102944_create_trades.rb
@@ -3,7 +3,7 @@ class CreateTrades < ActiveRecord::Migration
     create_table :trades do |t|
       t.string :name
       t.text :description
-      t.integer :ideas_count
+      t.integer :ideas_count, default: 0
 
       t.timestamps
     end
