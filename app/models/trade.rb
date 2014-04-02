8825fd66df3c90facac3e84d2059b9f8f266769d
Abdullah Alansari <ahimta@gmail.com>
2014-04-03 02:37:35 +0300

    added: initial setup for DRYing out coomon traits of all models

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,11 +1,33 @@
 class Trade < ActiveRecord::Base
   include Nameble
+  extend BaseModelable
 
-  ATTRIBUTES     = [:id] + Nameble::ATTRIBUTES + [:ideas_count,:shops_count,:workers_count,
-    :materials_count]
-  SAFE_PARAMS    = Nameble::ATTRIBUTES - [:id]
-  INVALID_TRAITS = Nameble::INVALID_TRAITS
-  VALID_TRAITS   = Nameble::VALID_TRAITS
+  def self.mixins
+    [Nameble]
+  end
+
+  base_modelable
+
+  def self.counter_caches
+    [:ideas_count,:shops_count,:workers_count,:materials_count]
+  end
+
+  def self.special_attrs
+    []
+  end
+
+  def self.special_valid_traits
+    []
+  end
+
+  def self.special_invalid_traits
+    []
+  end
+
+  ATTRIBUTES     = self.my_attributes
+  SAFE_PARAMS    = self.my_safe_params
+  INVALID_TRAITS = self.my_invalid_traits
+  VALID_TRAITS   = self.my_valid_traits
 
   has_many :ideas, dependent: :destroy
   has_many :shops, dependent: :destroy
