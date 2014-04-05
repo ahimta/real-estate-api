24787c9a09968c5f0d99a1d36600e222d74caade
Abdullah Alansari <ahimta@gmail.com>
2014-04-05 07:18:33 +0300

    changed: all models now use ModelProps class changed: Guardfile now runs controllers specs when their corresponding model changes

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,35 +1,11 @@
 class Shop < ActiveRecord::Base
-  extend BaseModelable
+  props = ModelProps.new(Shop, [Phonable,Priceble,Tradable,Notable,Namable,Ratable],
+    counter_caches: [:workers_count,:materials_count])
 
-  def self.mixins
-    [Phonable,Priceble,Tradable,Notable,Namable,Ratable]
-  end
-
-  base_modelable
-
-  class << self
-    def counter_caches
-      [:workers_count,:materials_count]
-    end
-
-    def special_attrs
-      []
-    end
-
-    def special_valid_traits
-      []
-    end
-
-    def special_invalid_traits
-      []
-    end
-  end
-
-
-  ATTRIBUTES     = self.my_attrs
-  SAFE_PARAMS    = self.my_safe_params
-  INVALID_TRAITS = self.my_invalid_traits
-  VALID_TRAITS   = self.my_valid_traits
+  ATTRIBUTES     = props.attrs
+  SAFE_PARAMS    = props.safe_params
+  INVALID_TRAITS = props.invalid_traits
+  VALID_TRAITS   = props.valid_traits
 
   has_many :workers, dependent: :destroy
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,34 +1,11 @@
 class Trade < ActiveRecord::Base
-  extend BaseModelable
+  props = ModelProps.new(Trade, [Namable],
+    counter_caches: [:ideas_count,:shops_count,:workers_count,:materials_count])
 
-  def self.mixins
-    [Namable]
-  end
-
-  base_modelable
-
-  class << self
-    def counter_caches
-      [:ideas_count,:shops_count,:workers_count,:materials_count]
-    end
-
-    def special_attrs
-      []
-    end
-
-    def special_valid_traits
-      []
-    end
-
-    def special_invalid_traits
-      []
-    end
-  end
-
-  ATTRIBUTES     = self.my_attrs
-  SAFE_PARAMS    = self.my_safe_params
-  INVALID_TRAITS = self.my_invalid_traits
-  VALID_TRAITS   = self.my_valid_traits
+  ATTRIBUTES     = props.attrs
+  SAFE_PARAMS    = props.safe_params
+  INVALID_TRAITS = props.invalid_traits
+  VALID_TRAITS   = props.valid_traits
 
   has_many :ideas, dependent: :destroy
   has_many :shops, dependent: :destroy
