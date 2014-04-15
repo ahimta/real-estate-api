6c059ff933e9156dc41d7f01ffc98a3f0001594a
Abdullah Alansari <ahimta@gmail.com>
2014-04-15 10:07:32 +0300

    fixed: enforced available locales to be used

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,9 @@ module RealEState
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:en, :ar]
     config.i18n.default_locale = :en
-    I18n.enforce_available_locales = true
+    config.i18n.enforce_available_locales = true
 
     config.middleware.use Rack::Cors do
       allow do
