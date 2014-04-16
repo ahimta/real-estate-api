624aaf09e7c6d3356081476b91ba3d0d6335815a
Abdullah Alansari <ahimta@gmail.com>
2014-04-16 10:03:43 +0300

    fixed: rspec tests in Guard notify failure when success

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,9 @@ CodeClimate::TestReporter.start
 
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-require 'rails/test_help'
 require 'rspec/rails'
 require 'rspec/autorun'
+# require 'rails/test_help'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -14,7 +14,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-# ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 module MyHelpers
   def get_record_attributes(record)
