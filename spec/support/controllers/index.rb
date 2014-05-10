e0dce944154517bdf47fd475a6ad2205ef422f4e
Abdullah Alansari <ahimta@gmail.com>
2014-05-10 03:48:25 +0300

    added: more specs for index actions to cover the case when there are no records refactored: index shared specs

--- spec/support/controllers/index.rb
+++ spec/support/controllers/index.rb
@@ -4,47 +4,52 @@ shared_examples 'controllers/index' do |model, resource, attributes, valid_trait
   describe '#index' do
 
     let(:index) { get :index, format: :json }
+    let(:tradables) {
+      {"#{resource}s"=>expected_records,'meta'=>{'parents'=>
+        {'trades'=> JSON.parse(Trade.select(:id, :name, *Trade.props.counter_caches).to_json)},
+        'pagination'=> pagination}}
+    }
+    let(:shopables) {
+      {"#{resource}s"=>expected_records,'meta'=>{'parents'=>
+        {'trades'=>JSON.parse(Trade.select(:id, :name, *Trade.props.counter_caches).to_json),
+        'shops'=>JSON.parse(Shop.select(:id, :name, *Shop.props.counter_caches).to_json)},
+        'pagination'=> pagination}}
+    }
+    let(:otherables) {{"#{resource}s" => expected_records, 'meta' => {'pagination' => pagination}}}
+    let(:test!) {
+      case resource
+      when 'idea', 'shop'
+        expect(json_response).to eq(tradables)
+      when 'worker'
+        expect(json_response).to eq(shopables)
+      else
+        expect(json_response).to eq(otherables)
+      end
+    }
+    let(:per_page) { Kaminari.config.default_per_page }
+    let(:all_records) {
+      persisted = records.sort { |r1, r2| r2.id <=> r1.id }
+      persisted.map { |record| get_record_attrs(record, attributes) }
+    }
+    let(:pagination) { {'page' => page, 'count' => count, 'per_page' => per_page, 'pages' => pages} }
+    let(:pages) { (count / per_page.to_f).ceil }
 
     context 'does not exist' do
+      let(:expected_records) { [] }
+      let(:page) { 1 }
+      let(:count) { 0 }
+
       before { expect(model.count).to be_zero }
 
+      it { get :index, page: page, format: :json }
       it { index }
 
+      after { expect(response.status).to eq(200) }
       after { expect(model.count).to be_zero }
+      after { test! }
     end
 
     context 'exist' do
-      let(:tradables) {
-        {"#{resource}s"=>expected_records,'meta'=>{'parents'=>
-          {'trades'=> JSON.parse(Trade.select(:id, :name, *Trade.props.counter_caches).to_json)},
-          'pagination'=> pagination}}
-      }
-      let(:shopables) {
-        {"#{resource}s"=>expected_records,'meta'=>{'parents'=>
-          {'trades'=>JSON.parse(Trade.select(:id, :name, *Trade.props.counter_caches).to_json),
-          'shops'=>JSON.parse(Shop.select(:id, :name, *Shop.props.counter_caches).to_json)},
-          'pagination'=> pagination}}
-      }
-      let(:otherables) {{"#{resource}s" => expected_records, 'meta' => {'pagination' => pagination}}}
-      let(:per_page) { Kaminari.config.default_per_page }
-      let(:all_records) {
-        persisted = records.sort { |r1, r2| r2.id <=> r1.id }
-        rs = persisted.map { |record| get_record_attrs(record, attributes) }
-        rs
-      }
-      let(:test!) {
-        case resource
-        when 'idea', 'shop'
-          expect(json_response).to eq(tradables)
-        when 'worker'
-          expect(json_response).to eq(shopables)
-        else
-          expect(json_response).to eq(otherables)
-        end
-      }
-      let(:pagination) { {'page' => page, 'count' => count, 'per_page' => per_page, 'pages' => pages} }
-      let(:pages) { (count / per_page.to_f).ceil }
-
       before { expect(model.count).to be_zero }
 
       after { expect(response.status).to eq(200) }
